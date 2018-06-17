0ea5693543d016640fcbd5274261ed2af44bb7d3
kevin lee <kl2695@nyu.edu>
2018-06-17 16:01:10 -0700

    submit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import OrderBook from './order_book.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
@@ -20,31 +20,37 @@ class App extends Component {
             <h1 className="w3-jumbo">Decentralized Token Exchange</h1>
           </div>
         </div>
-        <PlaceOrder/>
-        <FillOrder/>
-        <OrderBook/>
-        <div class="w3-third w3-container w3-black w3-large">
-          <h2>Contact Info</h2>
-          <p><i class="fa fa-map-marker"></i> San Francisco, US</p>
-          <p><i class="fa fa-phone" ></i> Phone: +00 00000</p>
-          <p><i class="fa fa-envelope"> </i> Email: mail@mail.com</p>
+        <div className="app-top">
+          <div className="app-left">
+            <PlaceOrder />
+            <FillOrder />
+          </div>
+          <OrderBook/>
         </div>
-        <div class="w3-third w3-center w3-large w3-dark-grey w3-text-white">
-          <h2>Contact Us</h2>
-          <p>If you have an idea.</p>
-          <p>What are you waiting for?</p>
+        <div className="app-bottom">
+          <div class="w3-third w3-container w3-black w3-large">
+            <h2>Contact Info</h2>
+            <p><i class="fa fa-map-marker"></i> San Francisco, US</p>
+            <p><i class="fa fa-phone" ></i> Phone: +00 00000</p>
+            <p><i class="fa fa-envelope"> </i> Email: mail@mail.com</p>
+          </div>
+          <div class="w3-third w3-center w3-large w3-dark-grey w3-text-white">
+            <h2>Contact Us</h2>
+            <p>If you have an idea.</p>
+            <p>What are you waiting for?</p>
+          </div>
+          <div class="w3-third w3-center w3-large w3-grey w3-text-white">
+            <h2>Like Us</h2>
+            <i class="w3-xlarge fa fa-facebook-official"></i><br />
+            <i class="w3-xlarge fa fa-pinterest-p"></i><br />
+            <i class="w3-xlarge fa fa-twitter"></i><br />
+            <i class="w3-xlarge fa fa-flickr"></i><br />
+            <i class="w3-xlarge fa fa-linkedin"></i>
+          </div>
+        <div class="w3-container w3-text-grey">
+          <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
         </div>
-        <div class="w3-third w3-center w3-large w3-grey w3-text-white">
-          <h2>Like Us</h2>
-          <i class="w3-xlarge fa fa-facebook-official"></i><br />
-          <i class="w3-xlarge fa fa-pinterest-p"></i><br />
-          <i class="w3-xlarge fa fa-twitter"></i><br />
-          <i class="w3-xlarge fa fa-flickr"></i><br />
-          <i class="w3-xlarge fa fa-linkedin"></i>
         </div>
-      <div class="w3-container w3-text-grey">
-        <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-      </div>
       </div>
     );
   }
--- src/order_book.js
+++ src/order_book.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Aqueduct } from 'aqueduct'; 
-
+import $ from 'jquery';
 
 
 class OrderBook extends Component {
@@ -9,8 +9,20 @@ class OrderBook extends Component {
     constructor(props){
         super(props); 
         this.state = {
+            baseToken: "0x",
             baseTokenAddress: "0xe41d2489571d322189246dafa5ebde1f4699f498",
-            quoteTokenAddress: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+            quoteTokenAddress: "0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0",
+            quoteToken: "EOS",
+            addressList: {
+                "EOS": "0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0",
+                "TRX": "0xf230b790e05390fc8295f4d3f60332c93bed42e2",
+                "VET": "0x628c92CFAb21C93820751C6278f5A0cEF745676A",
+                "ICX": "0xb5A5F22694352C15B00323844aD545ABb2B11028",
+                "BNB": "0xB8c77482e45F1F44dE1745F52C74426C631bDD52",
+                "OMG": "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07",
+                "ZIL": "0x05f4a42e251f2d52b8ed15E9FEdAacFcEF1FAD27",
+                "AE": "0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d",
+            },
             orderBook: [],
         };
         this.handleClick = this.handleClick.bind(this);
@@ -19,47 +31,82 @@ class OrderBook extends Component {
 
     componentDidMount(){
         Aqueduct.Initialize();
+        let orders, orderBook; 
+        let fn = this; 
+        (() => {
+            orders = new Aqueduct.Api.OrdersService().get({
+                networkId: 1,
+                baseTokenAddress: fn.state.baseTokenAddress,
+                quoteTokenAddress: fn.state.quoteTokenAddress,
+                sortOrder: "price",
+                includeExternal: true,
+            }).then(result => {
+                console.log(result);
+                orderBook = result.map(order => {
+                    return (
+                        <div>
+                            {order.price}
+                        </div>
+                    );
+                });
+                this.setState({ orderBook: orderBook });
+            });
+        })();
+    }
+
+    componentDidUpdate(prevProps,prevState){
+        if(this.state.quoteToken === prevState.quoteToken){
+           
+        }else{
+            Aqueduct.Initialize();
+            let orders, orderBook;
+            let fn = this;
+            (() => {
+                orders = new Aqueduct.Api.OrdersService().get({
+                    networkId: 1,
+                    baseTokenAddress: fn.state.baseTokenAddress,
+                    quoteTokenAddress: fn.state.quoteTokenAddress,
+                }).then(result => {
+                    console.log(result);
+                    orderBook = result.map(order => {
+                        return (
+                            <div>
+                                {order.price}
+                            </div>
+                        );
+                    });
+                    console.log(orderBook);
+                    this.setState({ orderBook: orderBook });
+                });
+            })();
+        }
     }
     
-    handleClick(){
-
+    handleClick(name){
+        this.state.addressList[name];
+        this.setState({
+            quoteToken: name,
+            quoteTokenAddress: this.state.addressList[name],
+        });
     }
 
     render() {
-
-let orders, orderBook; 
-let fn = this; 
-console.log(this.state.orderBook);
-
-if(this.state.orderBook){
-    if(this.state.orderBook.length === 0){
-    (() => {
-        orders = new Aqueduct.Api.OrdersService().get({
-            networkId: 1,
-            baseTokenAddress: fn.state.baseTokenAddress,
-            quoteTokenAddress: fn.state.quoteTokenAddress,
-            sortOrder: "price",
-            includeExternal: true,
-        }).then(result => {
-            console.log(result);
-            orderBook = result.map(order => {
-                return (
-                    <div>
-                        {order.price}
-                    </div>
-                );
-            });
-        });
-    })();
-        this.setState({ orderBook: orderBook });
-    }
-}
-
         
+        console.log(this.state);
         return (
-            <div>
+            <div className="order-book">
                 <p>orderbook</p>
-                {orderBook}
+                <form action="" method="post">
+                    <a name="EOS" onClick={() => this.handleClick("EOS")}>0x</a>
+                    <a name="TRX" onClick={() => this.handleClick("TRX")}>TRX</a>
+                    <a name="VET" onClick={() => this.handleClick("VET")}>VET</a>
+                    <a name="ICX" onClick={() => this.handleClick("ICX")}>ICX</a>
+                    <a name="BNB" onClick={() => this.handleClick("BNB")}>BNB</a>
+                    <a name="OMG" onClick={() => this.handleClick("OMG")}>OMG</a>
+                    <a name="ZIL" onClick={() => this.handleClick("ZIL")}>ZIL</a>
+                    <a name="AE" onClick={() => this.handleClick("AE")}>AE</a>
+                </form>
+                {this.state.orderBook}
             </div>
         );
     }
